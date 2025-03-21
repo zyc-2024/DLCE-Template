1a41bf2f7197d6113f51d33dc5f6882af73f1d9f
zyc-2024 <61992011@qq.com>
2025-03-21 21:46:24 +0800

    加入谱面偏移量+修复bug

--- Assets/#Template/[Scripts]/Guidance/ScoreReader.cs
+++ Assets/#Template/[Scripts]/Guidance/ScoreReader.cs
@@ -9,9 +9,9 @@ namespace DancingLineFanmade.Guidance
     [DisallowMultipleComponent]
     public class ScoreReader : MonoBehaviour
     {
-        [SerializeField] private TextAsset score;
         [SerializeField] private Player player;
-        [SerializeField] private int playerSpeed = 12;
+        [SerializeField] private TextAsset score;
+        [SerializeField] private float offset;
         [SerializeField] private List<float> hits;
 
         private List<string> hitPoints = new List<string>();
@@ -37,13 +37,14 @@ namespace DancingLineFanmade.Guidance
 
             var index = hitPoints.IndexOf("[HitObjects]");
             hitPoints.RemoveRange(0, index + 1);
+            hitPoints.RemoveAll(text => text == string.Empty);
 
             var result = hitPoints.Select(VARIABLE => VARIABLE.Remove(0, 8))
                 .Select(hit => hit.Remove(hit.Length - 13, 13)).ToList();
             hitPoints = result;
             foreach (var VARIABLE in hitPoints)
             {
-                hits.Add(int.Parse(VARIABLE) / 1000f);
+                hits.Add(int.Parse(VARIABLE) / 1000f + offset);
             }
 
             #endregion
@@ -54,6 +55,7 @@ namespace DancingLineFanmade.Guidance
             var startPos = player.startPosition;
             var firstDir = player.firstDirection;
             var secondDir = player.secondDirection;
+            var speed = player.levelData.speed;
             var hitParent = new GameObject("GuideBoxHolder-ScoreCreated");
             var count = 1;
 
@@ -79,8 +81,8 @@ namespace DancingLineFanmade.Guidance
 
                 focusedBox.transform.Translate(
                     i == 0
-                        ? new Vector3(0, hits[i] * playerSpeed, 0)
-                        : new Vector3(0, (hits[i] - hits[i - 1]) * playerSpeed, 0), Space.Self);
+                        ? new Vector3(0, hits[i] * speed, 0)
+                        : new Vector3(0, (hits[i] - hits[i - 1]) * speed, 0), Space.Self);
 
                 boxes.Add(focusedBox);
                 count++;
